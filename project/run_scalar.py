a522a4d521b9b45ecc17dcba8d832bf80e156053
Yegor <egortr2004@mail.ru>
2025-09-29 20:10:57 +0300

    t

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -10,8 +10,10 @@ import minitorch
 class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
-        # TODO: Implement for Task 1.5.
-        raise NotImplementedError("Need to implement for Task 1.5")
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        # Second hidden layer to output layer
+        self.layer3 = Linear(hidden_layers, 1)
 
     def forward(self, x):
         middle = [h.relu() for h in self.layer1.forward(x)]
